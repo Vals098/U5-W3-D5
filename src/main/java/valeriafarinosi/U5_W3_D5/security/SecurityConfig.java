ebdb84a4ed0c776286cadb8d0c38ffe2fa103feb
Vals098 <valeria.farinosi@gmail.com>
2026-07-17 15:49:26 +0200

    Update SecurityConfig.java

--- src/main/java/valeriafarinosi/U5_W3_D5/security/SecurityConfig.java
+++ src/main/java/valeriafarinosi/U5_W3_D5/security/SecurityConfig.java
@@ -2,11 +2,26 @@ package valeriafarinosi.U5_W3_D5.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
+
+        httpSecurity.formLogin(formLogin -> formLogin.disable());
+        httpSecurity.sessionManagement(sessions -> sessions.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        httpSecurity.csrf(csrf -> csrf.disable());
+        httpSecurity.authorizeHttpRequests(req -> req.requestMatchers("/**").permitAll());
+
+
+        return httpSecurity.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
