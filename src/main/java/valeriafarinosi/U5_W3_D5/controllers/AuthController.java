89ca17b9723c5cbcdca56b38d0f96bab26d474d1
Vals098 <valeria.farinosi@gmail.com>
2026-07-17 17:33:15 +0200

    refactor: create users and organizers endpoints moved as register in AuthController

--- src/main/java/valeriafarinosi/U5_W3_D5/controllers/AuthController.java
+++ src/main/java/valeriafarinosi/U5_W3_D5/controllers/AuthController.java
@@ -5,17 +5,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import valeriafarinosi.U5_W3_D5.entities.User;
 import valeriafarinosi.U5_W3_D5.payloads.LoginDTO;
+import valeriafarinosi.U5_W3_D5.payloads.requests.NewUserDTO;
 import valeriafarinosi.U5_W3_D5.services.AuthService;
+import valeriafarinosi.U5_W3_D5.services.UserService;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
     private final AuthService authService;
+    private final UserService userService;
 
-    public AuthController(AuthService authService) {
+    public AuthController(AuthService authService, UserService userService) {
         this.authService = authService;
+        this.userService = userService;
     }
 
     @PostMapping("/login")
@@ -24,4 +29,17 @@ public class AuthController {
         return authService.checkCredentialsAndGenerateToken(body);
 
     }
+
+    @PostMapping("/register")
+    public User register(@RequestBody @Valid NewUserDTO body) {
+        return userService.saveUser(body);
+
+    }
+
+    @PostMapping("/register_organizer")
+    public User registerOrganizer(@RequestBody @Valid NewUserDTO body) {
+        return userService.saveOrganizer(body);
+
+    }
+
 }
